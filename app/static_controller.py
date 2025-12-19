624f4de1f8d383c0fbda18bb1f21a112ade49cfc
Jacob Buelow <jdbuelow@gmail.com>
2025-12-19 17:18:27 -0600

    Add favicon

--- app/static_controller.py
+++ app/static_controller.py
@@ -1,5 +1,5 @@
 from litestar import Controller, get
-from litestar.response import Template
+from litestar.response import Template, Redirect
 
 
 class StaticController(Controller):
@@ -9,3 +9,7 @@ class StaticController(Controller):
     async def index(self) -> Template:
         return Template("index.html")
 
+
+    @get("/favicon.ico")
+    async def favicon(self) -> Redirect:
+        return Redirect("/static/favicon.ico")
