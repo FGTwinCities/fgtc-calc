e41a91b9ebbc64eff168757532a193b8c963db56
Jacob Buelow <jdbuelow@gmail.com>
2026-01-07 15:21:00 -0600

    Print enums on buildsheet

--- app/db/enum.py
+++ app/db/enum.py
@@ -8,10 +8,20 @@ class MemoryType(Enum):
     DDR4 = 4
     DDR5 = 5
 
+    def __str__(self):
+        return "DDR" + str(self.value)
+
 
 class StorageDiskType(Enum):
     HDD = "hdd"
     SSD = "ssd"
+
+    def __str__(self) -> str:
+        match self:
+            case StorageDiskType.HDD:
+                return "HDD"
+            case StorageDiskType.SSD:
+                return "SSD"
 
 
 class StorageDiskForm(Enum):
@@ -20,12 +30,34 @@ class StorageDiskForm(Enum):
     M2 = "m2"
     PCIE = "pcie"
 
+    def __str__(self) -> str:
+        match self:
+            case StorageDiskForm.INCH25:
+                return "2.5\""
+            case StorageDiskForm.INCH35:
+                return "3.5\""
+            case StorageDiskForm.M2:
+                return "m.2"
+            case StorageDiskForm.PCIE:
+                return "PCIe"
+            case _:
+                return str(self.value)
+
 
 class StorageDiskInterface(Enum):
     IDE = "ide"
     SAS = "sas"
     SATA = "sata"
     NVME = "nvme"
+
+    def __str__(self) -> str:
+        match self:
+            case StorageDiskInterface.IDE | StorageDiskInterface.SAS | StorageDiskInterface.SATA:
+                return str(self.value).upper()
+            case StorageDiskInterface.NVME:
+                return "NVMe"
+            case _:
+                return str(self.value)
 
 
 class WirelessNetworkingStandard(Enum):
@@ -34,6 +66,21 @@ class WirelessNetworkingStandard(Enum):
     AC = "ac"
     AX = "ax"
     BE = "be"
+
+    def __str__(self) -> str:
+        match self:
+            case WirelessNetworkingStandard.BG:
+                return "802.11b/g"
+            case WirelessNetworkingStandard.N:
+                return "WiFi 4 (n)"
+            case WirelessNetworkingStandard.AC:
+                return "WiFi 5 (ac)"
+            case WirelessNetworkingStandard.AX:
+                return "WiFi 6 (ax)"
+            case WirelessNetworkingStandard.BE:
+                return "WiFi 7 (be)"
+            case _:
+                return "802.11" + str(self.value)
 
 
 class BuildType(Enum):
