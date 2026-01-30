e459a44eda7cb5ca3fb808be45e6b149e4e47983
Jacob Buelow <jdbuelow@gmail.com>
2026-01-30 15:38:03 -0600

    Parse geekbench single core performance

--- app/extern/benchmark/geekbench/geekbench.py
+++ app/extern/benchmark/geekbench/geekbench.py
@@ -12,8 +12,13 @@ class GeekbenchComponentResult(BenchmarkComponentResult):
     geekbench_id: int = None
 
 
+@dataclass
+class GeekbenchCpuResult(GeekbenchComponentResult):
+    single_thread_score: int = None
+
+
 class GeekbenchDataSource(BenchmarkDataSource):
-    _cached_cpu_list: list[GeekbenchComponentResult] | None = None
+    _cached_cpu_list: list[GeekbenchCpuResult] | None = None
     _cached_gpu_list: list[GeekbenchComponentResult] | None = None
 
     def _create_session(self) -> ClientSession:
@@ -22,7 +27,7 @@ class GeekbenchDataSource(BenchmarkDataSource):
             trust_env=True,
         )
 
-    async def _fetch_cpu_list(self) -> list[GeekbenchComponentResult]:
+    async def _fetch_cpu_list(self) -> list[GeekbenchCpuResult]:
         results = []
 
         async with self._create_session() as session:
@@ -33,10 +38,11 @@ class GeekbenchDataSource(BenchmarkDataSource):
                 data = await response.json()
                 devices = data.get("devices", [])
                 for device in devices:
-                    results.append(GeekbenchComponentResult(
+                    results.append(GeekbenchCpuResult(
                         geekbench_id=device.get("id"),
                         name=device.get("name"),
                         score=device.get("multicore_score"),
+                        single_thread_score=device.get("score"),
                     ))
 
         return results
@@ -60,13 +66,13 @@ class GeekbenchDataSource(BenchmarkDataSource):
 
         return results
 
-    async def retrieve_cpu_list(self) -> list[BenchmarkComponentResult]:
+    async def retrieve_cpu_list(self) -> list[GeekbenchCpuResult]:
         if self._cached_cpu_list is None:
             self._cached_cpu_list = await self._fetch_cpu_list()
 
         return self._cached_cpu_list
 
-    async def retrieve_gpu_list(self) -> list[BenchmarkComponentResult]:
+    async def retrieve_gpu_list(self) -> list[GeekbenchComponentResult]:
         if self._cached_gpu_list is None:
             self._cached_gpu_list = await self._fetch_gpu_list()
 
@@ -82,19 +88,19 @@ class GeekbenchDataSource(BenchmarkDataSource):
         res = await self._search(retrieve_callable, query)
         return res[0] if len(res) > 0 else None
 
-    async def search_cpu(self, query: str = None) -> list[BenchmarkComponentResult]:
+    async def search_cpu(self, query: str = None) -> list[GeekbenchCpuResult]:
         return await self._search(self.retrieve_cpu_list, query)
 
-    async def find_cpu(self, query: str) -> BenchmarkComponentResult | None:
+    async def find_cpu(self, query: str) -> GeekbenchCpuResult | None:
         return await self._find(self.retrieve_cpu_list, query)
 
-    async def search_gpu(self, query: str = None) -> list[BenchmarkComponentResult]:
+    async def search_gpu(self, query: str = None) -> list[GeekbenchComponentResult]:
         return await self._search(self.retrieve_gpu_list, query)
 
-    async def find_gpu(self, query: str) -> BenchmarkComponentResult:
+    async def find_gpu(self, query: str) -> GeekbenchComponentResult:
         return await self._find(self.retrieve_gpu_list, query)
 
-    async def retrieve_cpu_by_id(self, id: int) -> GeekbenchComponentResult | None:
+    async def retrieve_cpu_by_id(self, id: int) -> GeekbenchCpuResult | None:
         for cpu in await self.retrieve_cpu_list():
             if not isinstance(cpu, GeekbenchComponentResult):
                 raise RuntimeError
@@ -115,7 +121,7 @@ class GeekbenchDataSource(BenchmarkDataSource):
         return None
 
     async def update_cpu(self, processor: Processor, rebind: bool = False) -> None:
-        result: GeekbenchComponentResult
+        result: GeekbenchCpuResult
         if not processor.geekbench_id or rebind:
             result = await self.find_cpu(processor.model)
             if result is None:
@@ -126,6 +132,7 @@ class GeekbenchDataSource(BenchmarkDataSource):
         processor.model = result.name
         processor.geekbench_id = result.geekbench_id
         processor.geekbench_multithread_score = result.score
+        processor.geekbench_single_thread_score = result.single_thread_score
 
     async def update_gpu(self, gpu: GraphicsProcessor, rebind: bool = False) -> None:
         result: GeekbenchComponentResult
