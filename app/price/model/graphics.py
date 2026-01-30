49a49bd37dc0c9260647d448bf15befc5bb82f14
Jacob Buelow <jdbuelow@gmail.com>
2026-01-30 15:55:05 -0600

    Use geekbench score to price via benchmark when enabled

--- app/price/model/graphics.py
+++ app/price/model/graphics.py
@@ -1,6 +1,7 @@
 from litestar.exceptions import ValidationException
 
 from app.db.model import GraphicsProcessor
+from app.lib.util import getenv_bool
 
 
 def graphics_model_func(x: float, a: float, b: float) -> float:
@@ -10,7 +11,13 @@ class GraphicsProcessorPricingModel:
     passmark_parameters = (1, 0)
 
     def compute(self, gpu: GraphicsProcessor) -> float:
-        if not gpu.passmark_score:
+        score: int
+        if getenv_bool("USE_PASSMARK_PRICINGMODEL", True):
+            score = gpu.passmark_score
+        else:
+            score = gpu.geekbench_score
+
+        if not score:
             raise ValidationException("GPU does not have a passmark score, cannot evaluate price.")
 
-        return graphics_model_func(gpu.passmark_score, *self.passmark_parameters)
+        return graphics_model_func(score, *self.passmark_parameters)
--- app/price/model/processor.py
+++ app/price/model/processor.py
@@ -1,6 +1,7 @@
 from litestar.exceptions import ValidationException
 
 from app.db.model import Processor
+from app.lib.util import getenv_bool
 
 
 def processor_model_func(x: float, a: float, b: float) -> float:
@@ -11,7 +12,13 @@ class ProcessorPricingModel:
     passmark_parameters = (1, 0)
 
     def compute(self, processor: Processor) -> float:
-        if not processor.passmark_multithread_score:
+        score: int
+        if getenv_bool("USE_PASSMARK_PRICINGMODEL", True):
+            score = processor.passmark_multithread_score
+        else:
+            score = processor.geekbench_multithread_score
+
+        if not score:
             raise ValidationException("Processor does not have a passmark score, cannot evaluate price.")
 
-        return processor_model_func(processor.passmark_multithread_score, *self.passmark_parameters)
+        return processor_model_func(score, *self.passmark_parameters)
